03ebd0426161410c68b7ce7f7cc4e46a83dce543
ArjunNandkishorTiwari <tiwari.ar@northeasetrn.edu>
2022-05-08 00:40:38 -0400

    Ordered Linear Search
    Signed-off-by: ArjunNandkishorTiwari <tiwari.ar@northeasetrn.edu>

---
+++ Searching/orderedLinearSearch.py
@@ -0,0 +1,26 @@
+def orderedLinearSearch(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+        if arr[i]>target:
+            return -1
+    return -1
+
+def orderedLinearSearch2(arr,target):
+    for i in range(0,len(arr),2):
+        if arr[i] == target:
+            return i
+        if arr[i]>target:
+            if arr[i-1] == target:
+                return i-1
+            else:
+                return -1
+    return -1
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+print(orderedLinearSearch(arr,5))
+print(orderedLinearSearch(arr,100))
+print(orderedLinearSearch2(arr,6))
+print(orderedLinearSearch(arr,100))
